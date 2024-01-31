ce1bde79258d9ce12b03c86316046f4fff58e0f3
Jeremy Magland <magland@users.noreply.github.com>
2024-01-31 20:24:54 +0000

    use dendro

--- scripts/d_000582.py
+++ scripts/d_000582.py
@@ -1,64 +1,154 @@
-import json
+from typing import List
 import os
-from dandi_nwb_meta.dandi_nwb_meta import load_dandi_nwb_meta_output
-from vis_construction.create_units_vis import create_units_vis
-from vis_construction.create_2d_tuning_curves_vis import create_2d_tuning_curves_vis
 from jinja2 import Environment, FileSystemLoader
+import dendro.client as den
 
 
 thisdir = os.path.dirname(os.path.abspath(__file__))
 
-num_assets_to_process = 10
+num_bins = 30
 
 
 def d_000582():
     dandiset_id = "000582"
-    json_fname = os.path.join(thisdir, f"d_{dandiset_id}.json")
-    if not os.path.exists(json_fname):
-        dandi_nwb_meta_dandiset = load_dandi_nwb_meta_output(dandiset_id)
-        if dandi_nwb_meta_dandiset is None:
-            raise Exception(f"Failed to load nwb meta output for {dandiset_id}")
-        out_assets = []
-        for i, asset in enumerate(dandi_nwb_meta_dandiset.nwb_assets):
-            out_assets.append(
-                {
-                    "path": asset.asset_path,
-                    # 'url': asset.download_url  # in the future the download_url will be available
-                    "url": f"https://api.dandiarchive.org/api/assets/{asset.asset_id}/download/",
-                    "visualizations": [],
-                }
-            )
-            if i + 1 >= num_assets_to_process:
-                break
-        for out_asset in out_assets:
-            print(f"Processing {out_asset['path']}")
-            url = out_asset["url"]
-            v_units = create_units_vis(url)
-            out_asset["visualizations"].append(
-                {
-                    "type": "units",
-                    "figurl": v_units.url(label="Units for " + out_asset["path"]),
-                }
-            )
-            v_2d_tuning_curves = create_2d_tuning_curves_vis(url)
-            out_asset["visualizations"].append(
-                {
-                    "type": "2d_tuning_curves",
-                    "figurl": v_2d_tuning_curves.url(label="2D tuning curves for " + out_asset["path"]),
-                }
-            )
+    project_id = "a7852166"
 
-        with open(json_fname, "w") as f:
-            json.dump({"dandiset_id": dandiset_id, "assets": out_assets}, f, indent=4)
+    # Load the project
+    project = den.load_project(project_id)
 
-    with open(json_fname, "r") as f:
-        data = json.load(f)
+    nwb_file_names = _get_nwb_file_paths(project, f"imported/{dandiset_id}")
+
+    files_out = []
+    for nwb_file_name in nwb_file_names:
+        nwb_file_name_2 = nwb_file_name[len(f"imported/{dandiset_id}/") :]
+        file_out = {"nwb_file_name": nwb_file_name_2, "visualizations": []}
+        print(f"Processing {nwb_file_name_2}")
+
+        v = vis_tuning_curves_2d(
+            project=project, nwb_file_name=nwb_file_name, dandiset_id=dandiset_id
+        )
+        file_out["visualizations"].append(v)
+
+        v = vis_units_vis(
+            project=project, nwb_file_name=nwb_file_name, dandiset_id=dandiset_id
+        )
+        file_out["visualizations"].append(v)
+
+        files_out.append(file_out)
+
+    data = {"dandiset_id": dandiset_id, "files": files_out}
 
     env = Environment(loader=FileSystemLoader(thisdir))
     template = env.get_template("d_000582.template.md")
-    output = template.render(**data)
+    file_out = template.render(**data)
     out_fname = os.path.join(thisdir, f"../generated/d_{dandiset_id}.md")
     if not os.path.exists(os.path.dirname(out_fname)):
         os.makedirs(os.path.dirname(out_fname))
     with open(out_fname, "w") as f:
-        f.write(output)
+        f.write(file_out)
+
+
+def vis_tuning_curves_2d(project: den.Project, nwb_file_name: str, dandiset_id: str):
+    nwb_file_name_2 = nwb_file_name[len(f"imported/{dandiset_id}/") :]
+    output_file_name = (
+        f"generated/{dandiset_id}/" + nwb_file_name_2 + "/tuning_curves_2d.nh5"
+    )
+    den.submit_job(
+        project=project,
+        processor_name="dendro1.tuning_curves_2d",
+        input_files=[den.SubmitJobInputFile(name="input", file_name=nwb_file_name)],
+        output_files=[
+            den.SubmitJobOutputFile(
+                name="output",
+                file_name=output_file_name,
+            )
+        ],
+        parameters=[
+            den.SubmitJobParameter(name="num_bins", value=num_bins),
+            den.SubmitJobParameter(
+                name="spatial_series_path",
+                value="processing/behavior/Position/SpatialSeriesLED1",
+            ),
+        ],
+        required_resources=den.DendroJobRequiredResources(
+            numCpus=2, numGpus=0, memoryGb=4, timeSec=60 * 60
+        ),
+        run_method="local",
+    )
+    f = project.get_file(output_file_name)
+    if f is None:
+        return {"type": "2d_tuning_curves", "status": "submitted"}
+    elif (
+        f is not None and f._file_data.content == "pending"
+    ):  # todo: expose this in the dendro API somehow
+        return {"type": "2d_tuning_curves", "status": "pending"}
+    else:
+        url = f.get_url()
+        figurl = f"https://figurl.org/f?v=https://figurl-tuning-curves-1.surge.sh&d=%7B%22type%22:%22tuning_curves_2d_nh5%22,%22nh5_file%22:%22{url}%22%7D&label={nwb_file_name_2}/tuning_curves_2d.nh5"
+        return {
+            "type": "2d_tuning_curves",
+            "status": "done",
+            "figurl": figurl,
+        }
+
+
+def vis_units_vis(project: den.Project, nwb_file_name: str, dandiset_id: str):
+    nwb_file_name_2 = nwb_file_name[len(f"imported/{dandiset_id}/") :]
+    output_file_name = (
+        f"generated/{dandiset_id}/" + nwb_file_name_2 + "/units_vis.figurl"
+    )
+    den.submit_job(
+        project=project,
+        processor_name="dendro1.units_vis",
+        input_files=[den.SubmitJobInputFile(name="input", file_name=nwb_file_name)],
+        output_files=[
+            den.SubmitJobOutputFile(
+                name="output",
+                file_name=output_file_name,
+            )
+        ],
+        parameters=[],
+        required_resources=den.DendroJobRequiredResources(
+            numCpus=2, numGpus=0, memoryGb=4, timeSec=60 * 60
+        ),
+        run_method="local",
+    )
+    f = project.get_file(output_file_name)
+    if f is None:
+        return {"type": "units", "status": "submitted"}
+    elif (
+        f is not None and f._file_data.content == "pending"
+    ):  # todo: expose this in the dendro API somehow
+        return {"type": "units", "status": "pending"}
+    else:
+        url = f.get_url()
+        figurl = _download_text(url)
+        return {
+            "type": "units",
+            "status": "done",
+            "figurl": figurl,
+        }
+
+
+def _download_text(url: str):
+    from urllib import request
+
+    req = request.Request(
+        url,
+        headers={"User-Agent": "Mozilla/5.0"},
+    )
+    with request.urlopen(req) as response:
+        return response.read().decode("utf-8")
+
+
+def _get_nwb_file_paths(project: den.Project, folder_path: str):
+    ret: List[str] = []
+    folder = project.get_folder(folder_path)
+    files = folder.get_files()
+    for file in files:
+        ret.append(file.file_name)
+    folders = folder.get_folders()
+    for f in folders:
+        a = _get_nwb_file_paths(project, f.path)
+        ret.extend(a)
+    return ret
