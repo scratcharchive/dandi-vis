c7ce43abc78e40614853882e8c75b6513a3a38ba
Jeremy Magland <jmagland@flatironinstitute.org>
2024-02-12 14:09:20 -0500

    ecephys_summary: write recording.dat

--- dendro_apps/dandi-vis-1/ecephys_summary/ecephys_summary.py
+++ dendro_apps/dandi-vis-1/ecephys_summary/ecephys_summary.py
@@ -33,8 +33,27 @@ class EcephysSummaryProcessor(ProcessorBase):
         context.input.download('recording.json')
 
         print('Loading recording...')
-        recording = si.load_extractor('recording.json')
-        assert isinstance(recording, si.BaseRecording), "Recording is not a BaseRecording"
+        recording1 = si.load_extractor('recording.json')
+        assert isinstance(recording1, si.BaseRecording), "Recording is not a BaseRecording"
+
+        print('Writing recording to .dat file...')
+        si.BinaryRecordingExtractor.write_recording(
+            recording=recording1,
+            file_paths=['recording.dat'],
+            dtype='float32',
+            n_jobs=1,
+            chunk_duration='5s'  # not sure how to best set this
+        )
+        print('Loading recording from .dat file...')
+        recording = si.BinaryRecordingExtractor(
+            file_paths=['recording.dat'],
+            sampling_frequency=recording1.get_sampling_frequency(),
+            channel_ids=recording1.get_channel_ids(),
+            num_channels=recording1.get_num_channels(),
+            dtype='float32'
+        )
+        print('Setting channel locations...')
+        recording.set_channel_locations(recording1.get_channel_locations())
 
         num_frames = recording.get_num_frames()
         bin_size_sec = 1 / 5
@@ -70,7 +89,6 @@ class EcephysSummaryProcessor(ProcessorBase):
                 bin_start = batches[ib]['bin_start']
                 bin_end = batches[ib]['bin_end']
                 num_bins_in_batch = bin_end - bin_start
-                print(f"Processing batch {ib} of {len(batches)}")
                 X = recording.get_traces(start_frame=bin_start * bin_size_frames, end_frame=bin_end * bin_size_frames)
                 X_reshaped = X.reshape((num_bins_in_batch, bin_size_frames, M)).astype(np.int16)
                 p_min[bin_start:bin_end, :] = np.min(X_reshaped, axis=1)
