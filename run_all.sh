326d0341e0098f53c739439659e555c901aa5a55
Jeremy Magland <magland@users.noreply.github.com>
2024-02-26 18:04:26 +0000

    update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# check that DENDRO_API_KEY is set
-if [ -z "$DENDRO_API_KEY" ]; then
-  echo "DENDRO_API_KEY is not set"
-  exit 1
-fi
+# # check that DENDRO_API_KEY is set
+# if [ -z "$DENDRO_API_KEY" ]; then
+#   echo "DENDRO_API_KEY is not set"
+#   exit 1
+# fi
 
 python dandisets/000582/run.py
 python dandisets/000784/run.py
